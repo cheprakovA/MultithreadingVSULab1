cd68f1774cd5974b317a3e977c5da5a7953ad33b
Artemy <artemy.cheprakov@gmail.com>
2019-09-19 00:18:51 +0300

    Initial Commit

---
+++ MultithreadingVSULab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MultithreadingVSULab1
+//
+//  Created by Artemy Cheprakov on 19/09/2019.
+//  Copyright © 2019 VSU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
